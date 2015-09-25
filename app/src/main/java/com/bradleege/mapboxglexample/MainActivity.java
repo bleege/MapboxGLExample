2c0715c5b6f2c04af9a751034b34bd0057705640
Brad Leege <bleege@gmail.com>
2015-09-25 14:46:43 -0500

    Basic setup of Mapbox MapView in Activity

--- app/src/main/java/com/bradleege/mapboxglexample/MainActivity.java
+++ app/src/main/java/com/bradleege/mapboxglexample/MainActivity.java
@@ -4,13 +4,65 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.mapbox.mapboxgl.views.MapView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mapView = (MapView) findViewById(R.id.mapboxMapView);
+        mapView.setAccessToken(getString(R.string.accessToken));
+        mapView.setStyleUrl(getString(R.string.style_url_mapbox_streets));
+        mapView.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    // Called when our app goes into the background
+    @Override
+    public void onPause()  {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    // Called when our app comes into the foreground
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    /**
+     * Save all appropriate fragment state.
+     *
+     * @param outState
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
 
     @Override
